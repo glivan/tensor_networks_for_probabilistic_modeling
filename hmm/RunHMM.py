af56162a6c1b367f338c9e46a5e2ad8472c0b66f
glivan <glasser_ivan@msn.com>
2019-04-09 15:44:33 +0200

    Added Hidden Markov Model training

---
+++ hmm/RunHMM.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 13 15:52:54 2018
+
+@author: Ivan
+"""
+
+import numpy as np
+import os.path
+import sys
+import pickle
+import time
+import pomegranate
+
+def init(datasetload_init='lymphography',
+         bond_dimension_init='2',n_iter_init='10',save_init=False):
+    """Initialize parameters :
+        ----------
+        datasetload : str, path of dataset
+        bond_dimension : int, bond dimension of MPS (number of hidden states)
+        n_iter : int, Number of iterations over the training dataset to perform
+    """
+    global datasetload
+    global bond_dimension
+    global n_iter
+    global save
+    
+    datasetload=str(datasetload_init)
+    bond_dimension=int(bond_dimension_init)
+    n_iter=int(n_iter_init)
+    save=bool(save_init)
+        
+def run():
+    # Load dataset
+    if os.path.isdir('/ptmp/mpq/iglasser/unsupervised'):
+        path='/ptmp/mpq/iglasser/unsupervised/'
+    else:
+        path='datasets/'
+    with open(path+datasetload, 'rb') as f:
+        a=pickle.load(f)
+    X=a[0]
+    X=X.astype(int)
+    
+    # Create HMM
+    D=bond_dimension
+    N=X.shape[1]
+    d=np.max(X+1)
+    list_of_states=[]
+    for i in xrange(N):
+        list_of_states.append([])
+        for u in xrange(bond_dimension):
+            dictionnary=dict()
+            for l in xrange(d):
+                dictionnary[str(l)] = np.random.rand()
+            list_of_states[i].append(pomegranate.State(pomegranate.DiscreteDistribution(dictionnary)))
+    model = pomegranate.HiddenMarkovModel()
+    for i in xrange(N-1):
+        for d in xrange(D):
+            for d2 in xrange(D):
+                model.add_transition(list_of_states[i][d],list_of_states[i+1][d2],np.random.rand())
+    for d in xrange(D):
+        model.add_transition(model.start,list_of_states[0][d],np.random.rand())
+    for d in xrange(D):
+        model.add_transition(list_of_states[N-1][d],model.end,np.random.rand())
+    model.bake()
+
+    # Train HMM
+    sequencetrain=[[str(i) for i in v] for v in X]
+    np.random.seed()
+    model.fit(sequencetrain,algorithm='baum-welch',stop_threshold=1e-50,min_iterations=1000,\
+              max_iterations=n_iter)
+    
+    u=0
+    for i in sequencetrain:
+        u+=model.log_probability(i)
+    accuracy=-u/len(sequencetrain)
+    
+    #train HMM
+    begin = time.time()  
+
+    time_elapsed = time.time()-begin
+    
+    print("Negative log likelihood = %.3f" % (accuracy))
+    print("Time elapsed = %.2fs" %(time_elapsed))
+
+    if save:
+        name='mpsresult'+'_'+str(datasetload)+'_'+'hmm'+'_'+\
+            str(bond_dimension)+'_'+str(n_iter)+'_'+str(accuracy)
+        with open(path+name, 'wb') as f:
+            pickle.dump([datasetload, bond_dimension, n_iter, accuracy], f)
+
+if __name__ == '__main__':
+    # Main program : initialize with options from command line and run
+    init(*sys.argv[1::])
+    run()
